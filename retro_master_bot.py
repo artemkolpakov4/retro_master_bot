6f1b3c1772c08d437fc4d6aeee23393935f226be
Artem Kolpakov <47293713+artemkolpakov4@users.noreply.github.com>
2024-11-05 15:36:16 +0300

    Update retro_master_bot.py

--- retro_master_bot.py
+++ retro_master_bot.py
@@ -4,7 +4,7 @@ import os
 import telegram
 
 TOKEN = os.getenv("5931866701:AAH_LpY3o5KMrWJhckoVTlfP2IwIA5ESkFU")
-CHAT_ID = os.getenv("CHAT_ID")
+CHAT_ID = os.getenv("-1001557949594")
 
-bot = telegram.Bot(token="-1001557949594")
+bot = telegram.Bot(token=TOKEN)
 bot.send_message(chat_id=CHAT_ID, text="Привет! Это напоминание.")
