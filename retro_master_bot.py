c79d1dcfd0ae4265a7c839b0991e543e30822a64
Artem Kolpakov <47293713+artemkolpakov4@users.noreply.github.com>
2024-11-05 15:54:21 +0300

    Update retro_master_bot.py

--- retro_master_bot.py
+++ retro_master_bot.py
@@ -2,6 +2,8 @@
 
 import os
 import telegram
+import asyncio
+import time
 
 TOKEN = "5931866701:AAH_LpY3o5KMrWJhckoVTlfP2IwIA5ESkFU"
 CHAT_ID = "-1001557949594"
@@ -11,4 +13,9 @@ async def send_message():
   message = 'Сообщение, которое нужно отправить'
   await bot.send_message(chat_id=CHAT_ID, text=message)
 
-send_message()
+async def main():
+    while True:
+        await send_message()
+        await asyncio.sleep(5)
+
+asyncio.run(main())
