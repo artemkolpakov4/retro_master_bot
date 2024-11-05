5f772cd46f3d9fab9e10b738531ab936a971cbaf
Artem Kolpakov <47293713+artemkolpakov4@users.noreply.github.com>
2024-11-05 15:39:24 +0300

    Update retro_master_bot.py

--- retro_master_bot.py
+++ retro_master_bot.py
@@ -5,6 +5,10 @@ import telegram
 
 TOKEN = os.getenv("5931866701:AAH_LpY3o5KMrWJhckoVTlfP2IwIA5ESkFU")
 CHAT_ID = os.getenv("-1001557949594")
-
 bot = telegram.Bot(token=TOKEN)
-bot.send_message(chat_id=CHAT_ID, text="Привет! Это напоминание.")
+
+async def send_message():
+  message = 'Сообщение, которое нужно отправить'
+  await bot.send_message(chat_id=CHAT_ID, text=message)
+
+send_message()
