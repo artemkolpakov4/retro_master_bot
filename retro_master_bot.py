51e3623863a9aa919bdd5dc31137169c731fbd92
Artem Kolpakov <47293713+artemkolpakov4@users.noreply.github.com>
2024-11-07 12:52:27 +0300

    Update retro_master_bot.py

--- retro_master_bot.py
+++ retro_master_bot.py
@@ -5,18 +5,19 @@ import telegram
 import asyncio
 import time
 from aiocron import crontab
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
 TOKEN = "5931866701:AAH_LpY3o5KMrWJhckoVTlfP2IwIA5ESkFU"
 CHAT_ID = "-1001557949594"
 bot = telegram.Bot(token=TOKEN)
 
-async def send_message():
+async def send_message_weekly_update_2():
   message = 'Обновление Weekly Update v2'
-  await bot.send_message(chat_id=CHAT_ID, text=message)
+  button = InlineKeyboardButton(text="YQL", url="https://yql.yandex-team.ru/Operations/672c8829d930343670edad06")
+  keyboard = InlineKeyboardMarkup([[button]])
+  await bot.send_message(chat_id=CHAT_ID, text=message, reply_markup=keyboard)
 
-@crontab('1 * * * *')
-async def scheduled_send_message():
-    await send_message()
+cron_job = crontab('1 * * * *')(send_message_weekly_update_2)
 
 async def main():
     while True:
